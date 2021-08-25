69c31a40cd9c16bad49cdb5a1172e265a91dabb7
Justin Gardner <justin@hirelofty.com>
2021-08-25 13:18:14 -0600

    Add Eslintrc File

---
+++ .eslintrc.js
@@ -0,0 +1,133 @@
+/* eslint-disable */
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    '@vue/airbnb',
+    'plugin:vue/recommended',
+  ],
+  plugins: [
+    'import',
+  ],
+  settings: {
+    'import/resolver': {
+      'alias': {
+        'map': [
+          ['@', '.src'],
+        ],
+        'extensions': [
+          '.vue',
+          '.json',
+          '.js',
+        ],
+      },
+    },
+  },
+  rules: {
+    'array-bracket-newline': [
+      'error', {
+        'multiline': true,
+        'minItems': 2,
+      },
+    ],
+    'arrow-parens': ['error', 'as-needed'],
+    'newline-after-var': ['error', 'always'],
+    'newline-before-return': 'error',
+    'newline-per-chained-call': [
+      'error', {
+        'ignoreChainWithDepth': 1,
+      },
+    ],
+    'object-curly-newline': [
+      'error', {
+        'ObjectExpression': {
+          'multiline': true,
+          'minProperties': 1,
+        },
+        'ObjectPattern': {
+          'multiline': true,
+          'minProperties': 1,
+        },
+        'ImportDeclaration': 'never',
+        'ExportDeclaration': {
+          'multiline': true,
+          'minProperties': 1,
+        },
+      },
+    ],
+    'object-property-newline': [
+      'error', {
+        'allowAllPropertiesOnSameLine': false,
+      },
+    ],
+    'operator-linebreak': [
+      'error',
+      'after', {
+        'overrides': {
+          '?': 'before',
+          ':': 'before',
+        },
+      },
+    ],
+    'padding-line-between-statements': [
+      'error', {
+        'blankLine': 'always',
+        'prev': 'block-like',
+        'next': '*',
+      },
+    ],
+    'semi': ['error', 'never'],
+    'space-before-function-paren': ['error', 'always'],
+    'vue/attributes-order': [
+      'error', {
+        'order': [
+          'DEFINITION',
+          'LIST_RENDERING',
+          'CONDITIONALS',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          ['UNIQUE', 'SLOT'],
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'OTHER_ATTR',
+          'EVENTS',
+          'CONTENT'
+        ],
+        'alphabetical': false
+      },
+    ],
+    'vue/html-indent': [
+      'error',
+      2, {
+        'attribute': 1,
+        'baseIndent': 1,
+        'closeBracket': 0,
+        'alignAttributesVertically': true,
+        'ignores': [],
+      },
+    ],
+    'vue/html-quotes': ['error', 'single'],
+    'vue/html-self-closing': [
+      'error', {
+        'html': {
+          'void': 'always',
+          'normal': 'always',
+          'component': 'always',
+        },
+        'svg': 'always',
+        'math': 'always',
+      },
+    ],
+    'vue/order-in-components': ['error'],
+    'vue/singleline-html-element-content-newline': [
+      'error', {
+        'ignoreWhenNoAttributes': false,
+        'ignoreWhenEmpty': true,
+        'ignores': [],
+      },
+    ],
+  },
+}
