e65ef32a23ed1e76df21ceb10cb14faeb97a768e
Justin Gardner <gardner.justin@gmx.com>
2023-09-10 09:13:03 -0600

    Mostly Replace Fish Config with Default from Garuda

--- config.fish
+++ config.fish
@@ -13,6 +13,7 @@ end
 set -U __done_min_cmd_duration 10000
 set -U __done_notification_urgency_level low
 
+
 ## Environment setup
 # Apply .profile: use this to put fish compatible .profile stuff in
 if test -f ~/.fish_profile
@@ -36,8 +37,12 @@ end
 
 ## Starship prompt
 if status --is-interactive
-   source ("/usr/local/bin/starship" init fish --print-full-init | psub)
+   source ("/usr/bin/starship" init fish --print-full-init | psub)
 end
+
+
+## Advanced command-not-found hook
+source /usr/share/doc/find-the-command/ftc.fish
 
 
 ## Functions
@@ -92,35 +97,68 @@ end
 
 ## Useful aliases
 # Replace ls with exa
-alias ll='exa -al --color=always --group-directories-first --icons'  # long format
+alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
+alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first --icons'  # long format
+alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
+alias l.='exa -ald --color=always --group-directories-first --icons .*' # show only dotfiles
+alias ip='ip -color'
+
+# Custom aliases
 alias git-rm-branches='git for-each-ref --format "%(refname:short)" refs/heads | grep -v "master\|main\|develop\|development" | xargs git branch -D'
-alias rebase='~/apps/scripts/rebase'
-alias cleanpush='~/apps/scripts/cleanpush'
 alias dc='docker-compose'
 
-# app aliases
-alias cropgen='cd ~/apps/cropgen/'
-alias bookkeeping='cd ~/apps/bookkeeping/'
-
 # Replace some more things with better alternatives
-alias cat='bat --style header --style rules --style snip --style changes --style header'
+alias cat='bat --style header --style snip --style changes --style header'
 [ ! -x /usr/bin/yay ] && [ -x /usr/bin/paru ] && alias yay='paru'
 
-set PATH /home/jgardner/.local/bin $PATH
+# Common use
+alias grubup="sudo update-grub"
+alias fixpacman="sudo rm /var/lib/pacman/db.lck"
+alias tarnow='tar -acf '
+alias untar='tar -xvf '
+alias wget='wget -c '
+alias rmpkg="sudo pacman -Rdd"
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+alias upd='/usr/bin/garuda-update'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='grep -F --color=auto'
+alias egrep='grep -E --color=auto'
+alias hw='hwinfo --short'                          # Hardware Info
+alias big="expac -H M '%m\t%n' | sort -h | nl"     # Sort installed packages according to size in MB
+alias gitpkg='pacman -Q | grep -i "\-git" | wc -l' # List amount of -git packages
 
-## Run paleofetch if session is interactive
-if status --is-interactive
-   neofetch
+# Get fastest mirrors
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
+alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
+alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
+
+# Help people new to Arch
+alias apt='man pacman'
+alias apt-get='man pacman'
+alias please='sudo'
+alias tb='nc termbin.com 9999'
+
+# Cleanup orphaned packages
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
+
+# Get the error messages from journalctl
+alias jctl="journalctl -p 3 -xb"
+
+# Recent installed packages
+alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+
+
+## Run fastfetch if session is interactive
+if status --is-interactive && type -q fastfetch
+   fastfetch --load-config dr460nized
 end
-
-# Generated for envman. Do not edit.
-test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
-
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/justin/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/justin/Downloads/google-cloud-sdk/path.fish.inc'; end
-
-# Bun
-set -Ux BUN_INSTALL "/Users/justin/.bun"
-set -px --path PATH "/Users/justin/.bun/bin"
-
